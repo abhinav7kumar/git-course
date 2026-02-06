f295f6f7e7ee3e652258ad38da2a359847e28009
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-06 12:07:07 +0530

    Allow checking multiple numbers using loop

--- main1.cpp
+++ main1.cpp
@@ -3,14 +3,22 @@ using namespace std;
 
 int main() {
     int num;
-    cout << "Enter a number: ";
-    cin >> num;
+    char choice;
 
-    if (num % 2 == 0) {
-        cout << num << " is Even";
-    } else {
-        cout << num << " is Odd";
-    }
+    do {
+        cout << "Enter a number: ";
+        cin >> num;
+
+        if (num % 2 == 0) {
+            cout << num << " is Even\n";
+        } else {
+            cout << num << " is Odd\n";
+        }
+
+        cout << "Do you want to check another number? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
 
     return 0;
 }
