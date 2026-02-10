f8c6927ee764e10cc29a5f756a8447b88aaf5262
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-10 12:08:31 +0530

    second version

--- chatbot.cpp
+++ chatbot.cpp
@@ -3,13 +3,20 @@ using namespace std;
 
 int main() {
     string input;
-    cout << "You: ";
-    cin >> input;
 
-    if (input == "hello")
-        cout << "Bot: Hello!" << endl;
-    else
-        cout << "Bot: I don't understand." << endl;
+    cout << "Type 'bye' to exit\n";
 
+    while (true) {
+        cout << "You: ";
+        cin >> input;
+
+        if (input == "hello")
+            cout << "Bot: Hi there!" << endl;
+        else if (input == "bye") {
+            cout << "Bot: Goodbye!" << endl;
+            break;
+        } else
+            cout << "Bot: Sorry, I don't understand." << endl;
+    }
     return 0;
 }
