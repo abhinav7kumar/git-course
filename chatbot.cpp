e2e65a6e7da7eae8db8d16855d2735c2c9545be1
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-10 12:11:31 +0530

    Refine fourth version

--- chatbot.cpp
+++ chatbot.cpp
@@ -2,34 +2,30 @@
 #include <string>
 using namespace std;
 
+void respond(string input) {
+    if (input.find("hello") != string::npos)
+        cout << "Bot: Hello! 😊\n";
+    else if (input.find("email") != string::npos)
+        cout << "Bot: I can help manage emails.\n";
+    else if (input.find("help") != string::npos)
+        cout << "Bot: Try asking about email or account.\n";
+    else
+        cout << "Bot: Could you rephrase that?\n";
+}
+
 int main() {
     string input;
-
-    cout << "Chatbot started (type 'bye' to exit)\n";
+    cout << "Chatbot Active (type 'bye' to exit)\n";
 
     while (true) {
         cout << "You: ";
         getline(cin, input);
 
-        if (input.find("hello") != string::npos)
-            cout << "Bot: Hello! How can I help you?\n";
-        else if (input.find("help") != string::npos)
-            cout << "Bot: I can answer basic questions.\n";
-        else if (input.find("bye") != string::npos) {
-            cout << "Bot: Goodbye!\n";
+        if (input.find("bye") != string::npos) {
+            cout << "Bot: Session ended.\n";
             break;
-        } else
-            cout << "Bot: I am still learning.\n";
+        }
+        respond(input);
     }
     return 0;
 }
-            cout << "Enter task number to delete: ";
-            cin >> index;
-
-            if (index > 0 && index <= tasks.size()) {
-                tasks.erase(tasks.begin() + index - 1);
-                cout << "Task deleted successfully.\n";
-            } else {
-                cout << "Invalid task number.\n";
-            }
-       
