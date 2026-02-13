42b967ea3430181906540df8a6a40140aacd73d8
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-13 22:03:51 +0530

    variables and data types

---
+++ home.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // 1️⃣ Variables and Data Types
+    cout << "1. Variables and Data Types\n";
+    int age = 22;
+    float cgpa = 8.4;
+    char grade = 'A';
+    bool isStudent = true;
+    cout << age << " " << cgpa << " " << grade << " " << isStudent << "\n\n";
