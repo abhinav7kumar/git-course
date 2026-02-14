0c4ea61e8bb9a3ba09172a9a0a0b86e8233a524c
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-14 23:10:10 +0530

    add higher or lower hints

--- numberGuessGame.cpp
+++ numberGuessGame.cpp
@@ -6,16 +6,19 @@ using namespace std;
 int main() {
     srand(time(0));
     int secret = rand() % 10 + 1;
-    int guess, attempts = 0;
+    int guess;
 
     while (true) {
         cout << "Guess (1-10): ";
         cin >> guess;
-        attempts++;
 
         if (guess == secret) {
-            cout << "Correct in " << attempts << " attempts!";
+            cout << "Correct!";
             break;
+        } else if (guess > secret) {
+            cout << "Too high\n";
+        } else {
+            cout << "Too low\n";
         }
     }
     return 0;
