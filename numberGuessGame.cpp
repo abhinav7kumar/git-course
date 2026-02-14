e9aa47cea2cf378626aa6f3dc9119c65fdbbfc5b
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-14 23:06:19 +0530

    take user input

--- numberGuessGame.cpp
+++ numberGuessGame.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main() {
     int secret = 7;
-    cout << "Guess the number!";
+    int guess;
+
+    cout << "Guess the number: ";
+    cin >> guess;
+
     return 0;
 }
