7d2d05e6db685db1d71919d5d8f7ae19543f087e
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-16 21:59:06 +0530

    add fixed puzzle number

--- numberlockgame.cpp
+++ numberlockgame.cpp
@@ -6,3 +6,9 @@ int main() {
     cout << "Guess the correct number to unlock." << endl;
     return 0;
 }
+
+int main() {
+    int secret = 5;
+    cout << "Guess the secret number (1-10)." << endl;
+    return 0;
+}
