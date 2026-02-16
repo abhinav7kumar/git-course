09b65a2a9f745ad46f3d89ed7869d04317c40f13
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-16 21:57:28 +0530

    create puzzle game introduction

---
+++ numberlockgame.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to the Puzzle Game!" << endl;
+    cout << "Guess the correct number to unlock." << endl;
+    return 0;
+}
