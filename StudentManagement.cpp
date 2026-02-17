3f91aec9f3db6a23bf107e93d23aeea2ac30b40c
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-17 22:03:14 +0530

    display student record

--- StudentManagement.cpp
+++ StudentManagement.cpp
@@ -15,4 +15,21 @@ int main() {
         cin >> s[i].name;
     }
     return 0;
+}#include <iostream>
+using namespace std;
+
+struct Student {
+    int roll;
+    string name;
+};
+
+int main() {
+    Student s[2];
+    for (int i = 0; i < 2; i++) {
+        cout << "Enter Roll: ";
+        cin >> s[i].roll;
+        cout << "Enter Name: ";
+        cin >> s[i].name;
+    }
+    return 0;
 }
