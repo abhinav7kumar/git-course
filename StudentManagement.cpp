cca037d1dda1995152b2c888c38c7aa3ba9f221e
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-17 21:58:44 +0530

    add basic menu options

--- StudentManagement.cpp
+++ StudentManagement.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    cout << "Student Management System";
+    int choice;
+    cout << "\n1. Add Student\n2. View Students\n3. Exit\n";
+    cout << "Enter choice: ";
+    cin >> choice;
     return 0;
 }
