5ed7af340ed144f6945ff1ed9ee5ed9251c969b7
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-17 22:00:09 +0530

    add student variables

--- StudentManagement.cpp
+++ StudentManagement.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int main() {
-    int choice;
-    cout << "\n1. Add Student\n2. View Students\n3. Exit\n";
-    cout << "Enter choice: ";
-    cin >> choice;
+    int roll;
+    string name;
+    cout << "Enter Roll: ";
+    cin >> roll;
+    cout << "Enter Name: ";
+    cin >> name;
     return 0;
 }
