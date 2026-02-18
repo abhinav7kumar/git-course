5bd060aa1c54506524188fc25692529aa0183d96
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-18 21:48:50 +0530

    initialize account balance

--- bankaccount.cpp
+++ bankaccount.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main() {
     string name;
     int accountNumber;
+    float balance = 0;
 
-    cout << "Enter account holder name: ";
+    cout << "Enter name: ";
     cin >> name;
     cout << "Enter account number: ";
     cin >> accountNumber;
 
+    cout << "Initial balance: " << balance << endl;
     return 0;
 }
