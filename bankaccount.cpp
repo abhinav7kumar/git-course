009e0ee8271ec7af787b1cc6217637c10c79acc5
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-18 21:51:25 +0530

    add withdrawl with balance check

--- bankaccount.cpp
+++ bankaccount.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main() {
-    float balance = 0, deposit;
+    float balance = 5000, withdraw;
 
-    cout << "Enter amount to deposit: ";
-    cin >> deposit;
+    cout << "Enter amount to withdraw: ";
+    cin >> withdraw;
 
-    balance += deposit;
-    cout << "Updated balance: " << balance << endl;
+    if (withdraw <= balance) {
+        balance -= withdraw;
+        cout << "Withdrawal successful.\n";
+    } else {
+        cout << "Insufficient balance.\n";
+    }
 
+    cout << "Current balance: " << balance << endl;
     return 0;
 }
