c9d38aa0542aee72c2c714df93155039910409ca
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-19 21:28:50 +0530

    add main menu options

--- librarySystem.c
+++ librarySystem.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Library Management System\n");
+    int choice;
+    printf("1. Add Book\n2. View Books\n3. Exit\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
     return 0;
 }
