e2faa79eff01d7cb13b13e254b1182665ce893a3
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-24 21:37:56 +0530

    Added dice rolling logic and player position variables

--- snakegame1.c
+++ snakegame1.c
@@ -6,8 +6,15 @@ void showWelcome() {
     printf("===== Snake and Ladder Game =====\n");
     printf("Two Player Game\n");
 }
-
+int rollDice() {
+    return (rand() % 6) + 1;
+}
 int main() {
+    srand(time(0));
+
+    int player1 = 0;
+    int player2 = 0;
+
     showWelcome();
     return 0;
 }
