8c956b042a73d2bcfb5965bd9bf3052437ebde74
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-24 21:36:35 +0530

    Setup basic snake and ladder structure

---
+++ snakegame1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void showWelcome() {
+    printf("===== Snake and Ladder Game =====\n");
+    printf("Two Player Game\n");
+}
+
+int main() {
+    showWelcome();
+    return 0;
+}
