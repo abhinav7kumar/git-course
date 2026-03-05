32dac04c9df1cdecac68ea3c4efa395433fa4675
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-03-05 21:27:54 +0530

    add task class

--- taskmanager1.py
+++ taskmanager1.py
@@ -1,3 +1,12 @@
+class Task:
+    def __init__(self, title):
+        self.title = title
+        self.completed = False
+
+    def mark_complete(self):
+        self.completed = True
+
+
 def main():
     print("Task Manager App")
 
