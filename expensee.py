8bcee51fa45ea29b5496285bb517c6ef1daf8614
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-03-07 21:43:14 +0530

    implement filtering expenses by category

--- expensee.py
+++ expensee.py
@@ -19,35 +19,23 @@ def add_expense():
     amount = float(input("Amount: "))
     category = input("Category: ")
 
-    expense = {
+    expenses.append({
         "name": name,
         "amount": amount,
         "category": category,
         "date": datetime.now().strftime("%Y-%m-%d")
-    }
-
-    expenses.append(expense)
+    })
 
 def view_expenses():
     for i, e in enumerate(expenses, start=1):
         print(f"{i}. {e['name']} | ₹{e['amount']} | {e['category']} | {e['date']}")
 
-def delete_expense():
-    view_expenses()
-    index = int(input("Delete number: ")) - 1
-    expenses.pop(index)
+def filter_category():
+    cat = input("Enter category: ")
 
-def show_summary():
-    total = sum(e["amount"] for e in expenses)
-    print("Total:", total)
-
-def category_summary():
-    summary = {}
     for e in expenses:
-        summary[e["category"]] = summary.get(e["category"], 0) + e["amount"]
-
-    for k, v in summary.items():
-        print(k, ":", v)
+        if e["category"] == cat:
+            print(e)
 
 def main():
     load_expenses()
@@ -55,24 +43,18 @@ def main():
     while True:
         print("\n1 Add")
         print("2 View")
-        print("3 Delete")
-        print("4 Total")
-        print("5 Category Summary")
-        print("6 Exit")
+        print("3 Filter Category")
+        print("4 Exit")
 
-        choice = input("Choice: ")
+        c = input("Choice: ")
 
-        if choice == "1":
+        if c == "1":
             add_expense()
-        elif choice == "2":
+        elif c == "2":
             view_expenses()
-        elif choice == "3":
-            delete_expense()
-        elif choice == "4":
-            show_summary()
-        elif choice == "5":
-            category_summary()
-        elif choice == "6":
+        elif c == "3":
+            filter_category()
+        elif c == "4":
             save_expenses()
             break
 
