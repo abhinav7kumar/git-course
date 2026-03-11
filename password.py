850b877b1ab9ed950b6733020e1e69d8e80479a6
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-03-11 16:08:23 +0530

    add random secured password generator

--- password.py
+++ password.py
@@ -15,10 +15,21 @@ def save_passwords():
     with open("vault.json", "w") as f:
         json.dump(passwords, f)
 
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(characters) for _ in range(length))
+
 def add_password():
-    site = input("Enter website/app name: ")
-    username = input("Enter username: ")
-    password = input("Enter password: ")
+    site = input("Website: ")
+    username = input("Username: ")
+
+    choice = input("Generate password? (y/n): ")
+
+    if choice.lower() == "y":
+        password = generate_password()
+        print("Generated password:", password)
+    else:
+        password = input("Enter password: ")
 
     passwords.append({
         "site": site,
@@ -29,33 +40,16 @@ def add_password():
     save_passwords()
 
 def view_passwords():
-    for i, entry in enumerate(passwords, start=1):
-        print(f"{i}. {entry['site']} | {entry['username']} | {entry['password']}")
-
-def delete_password():
-    view_passwords()
-    index = int(input("Delete number: ")) - 1
-
-    if 0 <= index < len(passwords):
-        passwords.pop(index)
-        save_passwords()
-
-def search_password():
-    site = input("Enter website to search: ")
-
     for entry in passwords:
-        if entry["site"].lower() == site.lower():
-            print(entry)
+        print(entry)
 
 def main():
     load_passwords()
 
     while True:
-        print("\n1 Add")
-        print("2 View")
-        print("3 Delete")
-        print("4 Search")
-        print("5 Exit")
+        print("\n1 Add Password")
+        print("2 View Passwords")
+        print("3 Exit")
 
         c = input("Choice: ")
 
@@ -64,10 +58,6 @@ def main():
         elif c == "2":
             view_passwords()
         elif c == "3":
-            delete_password()
-        elif c == "4":
-            search_password()
-        elif c == "5":
             break
 
 if __name__ == "__main__":
